249da698c08b0d1aa5199863fa8e9b1bb9b334de
Scott Burton <scottburton11@gmail.com>
2009-12-03 12:50:54 -0800

    Move the thumbnail tempfile code into processor, and provide a file reader

--- lib/seamus/processor.rb
+++ lib/seamus/processor.rb
@@ -1,10 +1,16 @@
 module Seamus
   class Processor
-    attr_reader :file_attributes
+    attr_reader :file, :file_attributes
 
     def initialize(file_path, file_attributes)
       @file = File.open(file_path, "r")
       @file_attributes = file_attributes
     end
+    
+    # nodoc
+    def thumb_tempfile
+      Tempfile.new("#{File.basename(@file.path, File.extname(@file.path))}_thumb.jpg")
+    end
+    
   end
 end
