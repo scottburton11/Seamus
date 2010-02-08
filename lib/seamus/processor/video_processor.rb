566b811743f2a958c21ebf295daf7b5de4e7a9cf
Scott Burton <scottburton11@gmail.com>
2010-02-08 13:43:41 -0800

    Uses duck-typing to get width, height and duration attributes; #aspect_ratio returns a Rational instead of a Float; improved spec coverage

--- lib/seamus/processor/video_processor.rb
+++ lib/seamus/processor/video_processor.rb
@@ -1,30 +1,18 @@
 module Seamus
-  module VideoProcessor# < Processor
+  module VideoProcessor
     
     def thumbnail
-      thumb = thumb_tempfile
-      IO.popen("ffmpeg -y -ss #{start_time} -i '#{@file.path}' -t 00:00:01 -vcodec mjpeg -vframes 1 -an -f rawvideo -s #{dimensions_string} - 2> /dev/null") do |io|
-        thumb.write io.read
-      end
-      thumb.close
-      return thumb
+      Open3.popen3("ffmpeg -y -ss #{start_time} -i '#{self.path}' -t 00:00:01 -vcodec mjpeg -vframes 1 -an -f rawvideo -s #{dimensions_string} - ")[1]
     end
     
     private
     
     def start_time
-      (file_attributes["duration"] || 10000)/3333
-    end
-    
-    # nodoc
-    def thumb_tempfile
-      Tempfile.new("#{File.basename(@file.path, File.extname(@file.path))}_thumb.jpg")
+      (duration || 10000)/3333
     end
     
     def aspect_ratio
-      width = (file_attributes["width"]).to_f
-      height = (file_attributes["height"]).to_f
-      return (width/height)
+      width.to_r/height.to_r
     end
     
     # For ffmpeg dimensions to work, the dimension components have to be even numbers,
